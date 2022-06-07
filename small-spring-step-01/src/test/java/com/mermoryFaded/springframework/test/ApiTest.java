2a1ddb5ba1140ae3d81870d48648534cdc696e11
memoryFade <707402918@qq.com>
2022-06-07 19:42:59 +0800

    第一节：简单的bean容器构建

---
+++ small-spring-step-01/src/test/java/com/mermoryFaded/springframework/test/ApiTest.java
@@ -0,0 +1,21 @@
+package com.mermoryFaded.springframework.test;
+
+
+import com.memoryFade.springframework.BeanDefinition;
+import com.memoryFade.springframework.BeanFactory;
+import com.mermoryFaded.springframework.test.bean.UserService;
+import org.junit.Test;
+
+public class ApiTest {
+    @Test
+    public void test_BeanFactory() {
+        //1. 初始化beanFactory
+        BeanFactory beanFactory = new BeanFactory();
+        //2. 注册bean
+        BeanDefinition beanDefinition = new BeanDefinition(new UserService());
+        beanFactory.registerBeanDefinition("userService", beanDefinition);
+        //3. 查询bean
+        UserService userService = (UserService) beanFactory.getBean("userService");
+        userService.queryUserInfo();
+    }
+}
