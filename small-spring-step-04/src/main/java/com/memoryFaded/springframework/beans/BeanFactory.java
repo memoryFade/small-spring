8212d13cc40fd42ff90cb5b55ec4fe5c009b0ac8
memoryFade <707402918@qq.com>
2022-06-08 16:29:16 +0800

    step:init

---
+++ small-spring-step-04/src/main/java/com/memoryFaded/springframework/beans/BeanFactory.java
@@ -0,0 +1,14 @@
+package com.memoryFaded.springframework.beans;
+
+public interface BeanFactory {
+    Object getBean(String name) throws BeansException;
+
+    /**
+     *  重载的方法，方便传递入参给构造函数实例化
+     * @param name
+     * @param args
+     * @return
+     * @throws BeansException
+     */
+    Object getBean(String name, Object... args) throws BeansException;
+}
