c0d07602155ed6f6d5b536ceaafb2fe01f5afb34
memoryFade <707402918@qq.com>
2022-06-08 19:06:31 +0800

    step04：为bean对象注入属性

--- small-spring-step-04/src/main/java/com/memoryFaded/springframework/beans/factory/config/BeanDefinition.java
+++ small-spring-step-04/src/main/java/com/memoryFaded/springframework/beans/factory/config/BeanDefinition.java
@@ -1,10 +1,21 @@
 package com.memoryFaded.springframework.beans.factory.config;
 
+import com.memoryFaded.springframework.beans.PropertyValues;
+
 public class BeanDefinition {
+
+
     private Class beanClass;
+    private PropertyValues propertyValues;
 
     public BeanDefinition(Class beanClass) {
         this.beanClass = beanClass;
+        this.propertyValues = new PropertyValues();
+    }
+
+    public BeanDefinition(Class beanClass, PropertyValues propertyValues) {
+        this.beanClass = beanClass;
+        this.propertyValues = propertyValues != null ? propertyValues : new PropertyValues();
     }
 
     public Class getBeanClass() {
@@ -13,5 +24,13 @@ public class BeanDefinition {
 
     public void setBeanClass(Class beanClass) {
         this.beanClass = beanClass;
+    }
+
+    public PropertyValues getPropertyValues() {
+        return propertyValues;
+    }
+
+    public void setPropertyValues(PropertyValues propertyValues) {
+        this.propertyValues = propertyValues;
     }
 }
--- small-spring-step-04/src/main/java/com/memoryFaded/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ small-spring-step-04/src/main/java/com/memoryFaded/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -1,7 +1,11 @@
 package com.memoryFaded.springframework.beans.factory.support;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.memoryFaded.springframework.beans.BeansException;
+import com.memoryFaded.springframework.beans.PropertyValue;
+import com.memoryFaded.springframework.beans.PropertyValues;
 import com.memoryFaded.springframework.beans.factory.config.BeanDefinition;
+import com.memoryFaded.springframework.beans.factory.config.BeanReference;
 
 import java.lang.reflect.Constructor;
 
@@ -15,10 +19,11 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
         Object bean = null;
         try {
             bean = createBeanInstance(beanDefinition,beanName,args);
+            applyPropertyValues(beanName,bean,beanDefinition);
         } catch (Exception e) {
             throw new BeansException("Instantiation of bean failed", e);
         }
-//
+
         addSingleton(beanName, bean);
         return bean;
     }
@@ -37,6 +42,20 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
         return getInstantiationStrategy().instantiate(beanDefinition,beanName,constructorToUse,args);
     }
 
+    private void applyPropertyValues(String beanName, Object bean, BeanDefinition beanDefinition) {
+        PropertyValues propertyValues = beanDefinition.getPropertyValues();
+        for (PropertyValue propertyValue : propertyValues.getPropertyValues()) {
+            String name = propertyValue.getName();
+            Object value = propertyValue.getValue();
+            if (value instanceof BeanReference) {
+                // A 依赖 B，获取 B 的实例化
+                BeanReference beanReference = ((BeanReference) value);
+                value = getBean(beanReference.getBeanName());
+            }
+
+            BeanUtil.setFieldValue(bean,name,value);
+        }
+    }
     public InstantiationStrategy getInstantiationStrategy() {
         return instantiationStrategy;
     }
