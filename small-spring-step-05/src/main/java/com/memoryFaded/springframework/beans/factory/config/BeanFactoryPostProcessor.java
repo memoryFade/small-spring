289a4678545bba0797121cc3ffe6065fc761124d
memoryFade <707402918@qq.com>
2022-06-09 21:22:23 +0800

    fixup! step06:定义BeanFactoryPostProcessor

---
+++ small-spring-step-05/src/main/java/com/memoryFaded/springframework/beans/factory/config/BeanFactoryPostProcessor.java
@@ -0,0 +1,8 @@
+package com.memoryFaded.springframework.beans.factory.config;
+
+import com.memoryFaded.springframework.beans.BeansException;
+import com.memoryFaded.springframework.beans.factory.ConfigurableListableBeanFactory;
+
+public interface BeanFactoryPostProcessor {
+    void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException;
+}
