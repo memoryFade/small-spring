a1a9d54df03718701c12089281f969e6f6b676e4
memoryFade <707402918@qq.com>
2022-06-10 18:46:04 +0800

    step08:init:copy from step07

---
+++ small-spring-step-08/src/main/java/com/memoryFaded/springframework/context/ConfigurableApplicationContext.java
@@ -0,0 +1,16 @@
+package com.memoryFaded.springframework.context;
+
+import com.memoryFaded.springframework.beans.BeansException;
+
+public interface ConfigurableApplicationContext extends ApplicationContext{
+
+    /**
+     * 刷新容器
+     * @throws BeansException
+     */
+    void refresh() throws BeansException;
+
+    void registerShutdownHook();
+
+    void close();
+}
