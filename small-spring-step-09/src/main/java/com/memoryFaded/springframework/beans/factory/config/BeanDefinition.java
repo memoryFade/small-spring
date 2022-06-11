c71404305a99d0f9cb6e10adea91df22f9988404
memoryFade <707402918@qq.com>
2022-06-11 11:25:11 +0800

    step09:add:Bean的作用范围定义和XML解析

--- small-spring-step-09/src/main/java/com/memoryFaded/springframework/beans/factory/config/BeanDefinition.java
+++ small-spring-step-09/src/main/java/com/memoryFaded/springframework/beans/factory/config/BeanDefinition.java
@@ -1,14 +1,49 @@
 package com.memoryFaded.springframework.beans.factory.config;
 
 import com.memoryFaded.springframework.beans.PropertyValues;
+import com.memoryFaded.springframework.beans.factory.ConfigurableBeanFactory;
 
 public class BeanDefinition {
+
+    String SCOPE_SINGLETON = ConfigurableBeanFactory.SCOPE_SINGLETON;
+
+    String SCOPE_PROTOTYPE = ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
     private Class beanClass;
     private PropertyValues propertyValues;
 
     private String initMethodName;
 
     private String destroyMethodName;
+
+    private String scope = SCOPE_SINGLETON;
+
+    private boolean singleton = true;
+    private boolean prototype = false;
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public boolean isSingleton() {
+        return singleton;
+    }
+
+    public void setSingleton(boolean singleton) {
+        this.singleton = singleton;
+    }
+
+    public boolean isPrototype() {
+        return prototype;
+    }
+
+    public void setPrototype(boolean prototype) {
+        this.prototype = prototype;
+    }
 
     public BeanDefinition(Class beanClass) {
         this.beanClass = beanClass;
