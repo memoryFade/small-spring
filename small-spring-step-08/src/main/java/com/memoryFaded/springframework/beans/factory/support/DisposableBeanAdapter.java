2b9bba21c97ea5096deca9f17252004f130c43d7
memoryFade <707402918@qq.com>
2022-06-11 10:16:02 +0800

    step08:fix:修复销毁方法由于判断逻辑的问题没有执行的问题

--- small-spring-step-08/src/main/java/com/memoryFaded/springframework/beans/factory/support/DisposableBeanAdapter.java
+++ small-spring-step-08/src/main/java/com/memoryFaded/springframework/beans/factory/support/DisposableBeanAdapter.java
@@ -28,7 +28,7 @@ public class DisposableBeanAdapter implements DisposableBean {
         }
 
         //2. 配置信息 destroy-method{判断是为了避免二次执行销毁}
-        if (StrUtil.isNotEmpty(destroyMethodName)&&!(bean instanceof  DisposableBean)&&"destroy".equals(this.destroyMethodName)){
+        if (StrUtil.isNotEmpty(destroyMethodName)&&!(bean instanceof  DisposableBean&&"destroy".equals(this.destroyMethodName))){
             Method destroyMethod = bean.getClass().getMethod(destroyMethodName);
             if (null == destroyMethod) {
                 throw new BeansException("Couldn't find a destroy method named '" + destroyMethodName + "' on bean with name '" + beanName + "'");
